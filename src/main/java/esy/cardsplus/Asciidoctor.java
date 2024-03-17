271c0ff7a6328e4f6d6646f2ca32d863e4f63a9d
robert.bruckbauer <robert.bruckbauer@e-mundo.de>
2024-03-17 17:43:00 +0100

    Dokumentation für den Test

---
+++ src/main/java/esy/cardsplus/Asciidoctor.java
@@ -0,0 +1,10 @@
+package esy.cardsplus;
+
+/**
+ * Asciidoctor is a fast, open source, Ruby-based text processor 
+ * for parsing AsciiDoc® into a document model and converting it 
+ * to output formats such as HTML 5, DocBook 5, manual pages, PDF, 
+ * EPUB 3, and other formats.
+ **/
+public class Cardsplus {    
+}
